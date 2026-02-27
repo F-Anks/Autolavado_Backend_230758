756fd3927b08ab09f7ccebbf1d8f17215e3d862d
F-Anks <franciscogarciagarcia911@gmail.com>
2026-02-27 11:33:44 -0600

    API

--- models/model_rol.py
+++ models/model_rol.py
@@ -1,6 +1,7 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, event
 from sqlalchemy.orm import relationship
 from config.db import Base
+import datetime
 
 class Rol(Base):
     __tablename__ = "tbc_roles"
@@ -12,3 +13,17 @@ class Rol(Base):
     fecha_actualizacion = Column(DateTime)
 
     usuarios = relationship("Usuario", back_populates="rols")
+
+# --- Lógica para insertar el primer rol automáticamente ---
+@event.listens_for(Rol.__table__, 'after_create')
+def insert_initial_values(target, connection, **kw):
+    now = datetime.datetime.now()
+    connection.execute(
+        target.insert().values(
+            Id=1,
+            nombre_rol="Admin",
+            estado=True,
+            fecha_registro=now,
+            fecha_actualizacion=now
+        )
+    )
